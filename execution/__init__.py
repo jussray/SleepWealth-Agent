476d2dbfc8b64651cadebe4d5c17265c61d89df3
Juss <286642846+jussray@users.noreply.github.com>
2026-09-13 18:32:00 -0400

    feat: export execution mode contract

--- execution/__init__.py
+++ execution/__init__.py
@@ -1,3 +1,19 @@
 from .executor import ExecutionManager
+from .modes import (
+    LIVE_MODE,
+    PRACTICE_MODE,
+    ExecutionMode,
+    get_execution_mode,
+    require_real_execution,
+    require_simulated_execution,
+)
 
-__all__ = ["ExecutionManager"]
+__all__ = [
+    "ExecutionManager",
+    "ExecutionMode",
+    "LIVE_MODE",
+    "PRACTICE_MODE",
+    "get_execution_mode",
+    "require_real_execution",
+    "require_simulated_execution",
+]
